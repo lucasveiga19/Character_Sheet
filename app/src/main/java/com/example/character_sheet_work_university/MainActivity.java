90c5ce1fe74191b12a3c2122034d149002f05aba
Lucas Uchôa da Veiga <lucas.veiga@laveiga.com.br>
2023-09-19 13:35:07 -0300

    Start Project

---
+++ app/src/main/java/com/example/character_sheet_work_university/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.character_sheet_work_university;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
